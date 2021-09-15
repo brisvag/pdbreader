ba3cf5334d6b2ac268158b88632ca9bc9f63beb6
Lorenzo Gaifas <brisvag@gmail.com>
2021-09-15 20:41:23 +0200

    tmp

--- pdbreader/parser.py
+++ pdbreader/parser.py
@@ -15,7 +15,10 @@ def parse_line(line):
     for record_type, spec in SPECIFICATION.items():
         if line.startswith(record_type):
             items = []
+            if not spec:
+                record_type = 'TER'
             for rng, dtype, _ in spec:
+                # split the line based on the specs
                 field = line[rng[0]:rng[1]]
                 try:
                     field = dtype(field.strip())
@@ -30,11 +33,16 @@ def parse_pdb(path):
     with open(path, 'r') as f:
         lines = f.readlines()
 
-    data = defaultdict(list)
+    data = [defaultdict(list)]
+    mol_idx = 0
+
     for line in lines:
+        data[mol_idx] = defaultdict(list)
         record_type, fields = parse_line(line)
-        if record_type is not None:
-            data[record_type].append(fields)
+        if record_type == 'TER':
+            mol_idx += 1
+        elif record_type is not None:
+            data[mol_idx][record_type].append(fields)
 
     dfs = defaultdict(list)
     for record_type, fields in data.items():
--- pdbreader/specification.py
+++ pdbreader/specification.py
@@ -1,34 +1,41 @@
-SPECIFICATION = {
-    'ATOM': [
-        ((6, 11), int, 'id'),
-        ((12, 16), str, 'name'),
-        ((16, 17), str, 'loc_indicator'),
-        ((17, 20), str, 'resname'),
-        ((21, 22), str, 'chain'),
-        ((22, 26), int, 'resid'),
-        ((26, 27), str, 'res_insertion_code'),
-        ((30, 38), float, 'x'),
-        ((38, 46), float, 'y'),
-        ((46, 54), float, 'z'),
-        ((54, 60), float, 'occupancy'),
-        ((60, 66), float, 'b_factor'),
-        ((72, 76), str, 'segment'),
-        ((76, 78), str, 'element'),
-        ((78, 80), str, 'charge'),
-    ],
-    'CONECT': [
-        ((6, 11), int, 'parent'),
-        ((11, 16), int, 'bond1'),
-        ((16, 21), int, 'bond2'),
-        ((21, 26), int, 'bond3'),
-        ((26, 31), int, 'bond4'),
-        ((31, 36), int, 'h_bond1'),
-        ((36, 41), int, 'h_bond2'),
-        ((41, 46), int, 'salt_bridge1'),
-        ((46, 51), int, 'h_bond3'),
-        ((51, 56), int, 'h_bond4'),
-        ((56, 61), int, 'salt_bridge2'),
-    ],
-}
+ATOM = [
+    ((6, 11), int, 'id'),
+    ((12, 16), str, 'name'),
+    ((16, 17), str, 'loc_indicator'),
+    ((17, 20), str, 'resname'),
+    ((21, 22), str, 'chain'),
+    ((22, 26), int, 'resid'),
+    ((26, 27), str, 'res_insertion_code'),
+    ((30, 38), float, 'x'),
+    ((38, 46), float, 'y'),
+    ((46, 54), float, 'z'),
+    ((54, 60), float, 'occupancy'),
+    ((60, 66), float, 'b_factor'),
+    ((72, 76), str, 'segment'),
+    ((76, 78), str, 'element'),
+    ((78, 80), str, 'charge'),
+]
 
-SPECIFICATION['HETATM'] = SPECIFICATION['ATOM']
+CONECT = [
+    ((6, 11), int, 'parent'),
+    ((11, 16), int, 'bond1'),
+    ((16, 21), int, 'bond2'),
+    ((21, 26), int, 'bond3'),
+    ((26, 31), int, 'bond4'),
+    ((31, 36), int, 'h_bond1'),
+    ((36, 41), int, 'h_bond2'),
+    ((41, 46), int, 'salt_bridge1'),
+    ((46, 51), int, 'h_bond3'),
+    ((51, 56), int, 'h_bond4'),
+    ((56, 61), int, 'salt_bridge2'),
+]
+
+SPECIFICATION = {
+    'ATOM': ATOM,
+    'HETATM': ATOM,
+    'CONECT': CONECT,
+    # all the following should end the molecule or model
+    'TER': [],
+    'END': [],
+    'ENDMDL': [],
+}
