ede1bc56edb36c04c27e90fec90eacd544954ca8
Lorenzo Gaifas <brisvag@gmail.com>
2021-09-16 13:16:25 +0200

    working parser

--- pdbreader/parser.py
+++ pdbreader/parser.py
@@ -5,7 +5,7 @@ import pandas as pd
 from .specification import SPECIFICATION
 
 
-def parse_line(line):
+def parse_line(line, mdl_idx=0):
     """
     slice a string in specific spots (must include edges,
     left inclusive and right exclusive) and return appropriate types
@@ -15,16 +15,23 @@ def parse_line(line):
     for record_type, spec in SPECIFICATION.items():
         if line.startswith(record_type):
             items = []
-            if not spec:
-                record_type = 'TER'
-            for rng, dtype, _ in spec:
-                # split the line based on the specs
-                field = line[rng[0]:rng[1]]
-                try:
-                    field = dtype(field.strip())
-                except ValueError:
-                    field = None
-                items.append(field)
+            for rng, dtype, colname in spec:
+                if colname == 'model_id':
+                    # special case for coordinates, add the model id
+                    items.append(mdl_idx)
+                else:
+                    # split the line based on the specs
+                    field = line[rng[0]:rng[1]]
+                    if colname == 'bonds':
+                        # special case for CONECT records, split the field in a list
+                        field = [field[i * 5: (i + 1) * 5].strip() for i in range(9)]
+                        items.append([int(f) for f in field if f != ''])
+                        continue
+                    try:
+                        field = dtype(field.strip()) or None
+                    except ValueError:
+                        field = None
+                    items.append(field)
             return record_type, items
     return None, None
 
@@ -33,20 +40,23 @@ def parse_pdb(path):
     with open(path, 'r') as f:
         lines = f.readlines()
 
-    data = [defaultdict(list)]
-    mol_idx = 0
-
+    records = defaultdict(list)
+    mdl_idx = 0
+    # parse contents
     for line in lines:
-        data[mol_idx] = defaultdict(list)
-        record_type, fields = parse_line(line)
-        if record_type == 'TER':
-            mol_idx += 1
+        record_type, fields = parse_line(line, mdl_idx)
+        if record_type == 'MODEL':
+            mdl_idx = fields[0]
+        elif record_type == 'ENDMDL':
+            mdl_idx += 1
         elif record_type is not None:
-            data[mol_idx][record_type].append(fields)
+            records[record_type].append(fields)
 
-    dfs = defaultdict(list)
-    for record_type, fields in data.items():
+    # put in dataframes with appropriate column names
+    data = {}
+    for record_type, fields in records.items():
         _, _, columns = zip(*SPECIFICATION[record_type])
         df = pd.DataFrame(fields, columns=columns)
-        dfs[record_type] = df
-    return dict(dfs)
+        data[record_type] = df
+
+    return dict(data)
--- pdbreader/specification.py
+++ pdbreader/specification.py
@@ -1,11 +1,12 @@
 ATOM = [
+    ((0, 0), None, 'model_id'),  # special empty column to add model id
     ((6, 11), int, 'id'),
     ((12, 16), str, 'name'),
     ((16, 17), str, 'loc_indicator'),
     ((17, 20), str, 'resname'),
     ((21, 22), str, 'chain'),
     ((22, 26), int, 'resid'),
-    ((26, 27), str, 'res_insertion_code'),
+    ((26, 27), str, 'res_icode'),
     ((30, 38), float, 'x'),
     ((38, 46), float, 'y'),
     ((46, 54), float, 'z'),
@@ -18,24 +19,75 @@ ATOM = [
 
 CONECT = [
     ((6, 11), int, 'parent'),
-    ((11, 16), int, 'bond1'),
-    ((16, 21), int, 'bond2'),
-    ((21, 26), int, 'bond3'),
-    ((26, 31), int, 'bond4'),
-    ((31, 36), int, 'h_bond1'),
-    ((36, 41), int, 'h_bond2'),
-    ((41, 46), int, 'salt_bridge1'),
-    ((46, 51), int, 'h_bond3'),
-    ((51, 56), int, 'h_bond4'),
-    ((56, 61), int, 'salt_bridge2'),
+    ((11, 61), list, 'bonds'),
+]
+
+MODEL = [
+    ((11, 14), int, 'model_id'),
+]
+
+HELIX = [
+    ((7, 10), int, 'seq'),
+    ((11, 14), str, 'helix_id'),
+    ((15, 18), str, 'init_resname'),
+    ((19, 20), str, 'init_chain'),
+    ((21, 25), int, 'init_resid'),
+    ((25, 26), str, 'init_icode'),
+    ((27, 30), str, 'end_resname'),
+    ((31, 32), str, 'end_chain'),
+    ((33, 37), int, 'end_resid'),
+    ((37, 38), str, 'end_icode'),
+    ((38, 40), int, 'class'),
+    ((40, 70), str, 'comment'),
+    ((71, 76), int, 'length'),
+]
+
+SHEET = [
+    ((7, 10), int, 'seq'),
+    ((11, 14), str, 'sheet_id'),
+    ((14, 16), int, 'num_strands'),
+    ((17, 20), str, 'init_resname'),
+    ((21, 22), str, 'init_chain'),
+    ((22, 26), int, 'init_resid'),
+    ((26, 27), str, 'init_icode'),
+    ((28, 31), str, 'end_resname'),
+    ((32, 33), str, 'end_chain'),
+    ((33, 37), int, 'end_resid'),
+    ((37, 38), str, 'end_icode'),
+    ((38, 40), int, 'sense'),
+    ((41, 45), str, 'curr_atom'),
+    ((45, 48), str, 'curr_resname'),
+    ((49, 50), str, 'curr_chain'),
+    ((50, 54), int, 'curr_resid'),
+    ((54, 55), str, 'curr_icode'),
+    ((56, 60), str, 'prev_atom'),
+    ((60, 63), str, 'prev_resname'),
+    ((64, 65), str, 'prev_chain'),
+    ((65, 69), int, 'prev_resid'),
+    ((69, 70), str, 'prev_icode'),
+]
+
+TURN = [
+    ((7, 10), int, 'seq'),
+    ((11, 14), str, 'turn_id'),
+    ((15, 18), str, 'init_resname'),
+    ((19, 20), str, 'init_chain'),
+    ((20, 24), int, 'init_resid'),
+    ((24, 25), str, 'init_icode'),
+    ((26, 29), str, 'end_resname'),
+    ((30, 31), str, 'end_chain'),
+    ((31, 35), int, 'end_resid'),
+    ((35, 36), str, 'end_icode'),
+    ((40, 70), str, 'comment'),
 ]
 
 SPECIFICATION = {
     'ATOM': ATOM,
     'HETATM': ATOM,
     'CONECT': CONECT,
-    # all the following should end the molecule or model
-    'TER': [],
-    'END': [],
+    'MODEL': MODEL,
+    'HELIX': HELIX,
+    'SHEET': SHEET,
+    'TURN': TURN,
     'ENDMDL': [],
 }
